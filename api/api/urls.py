1b5f67d2cacc8913ebea876dc9132bd36e44550e
Samrath Srivastava <samrath.srivastava@webileapps.com>
2024-11-04 16:47:31 +0530

    changes in api urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
    #  path('api/auth/', include('authentication.urls')),  # Authentication endpoints
-    path('api/', include('pharma.urls')),        # Other API endpoints
+    path('api/', include('api.pharma.urls')),        # Other API endpoints
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
